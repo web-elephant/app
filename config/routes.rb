7582e561cb038c34c80d0d82cf420b92a8e334d5
toshiaki <toshi.443.aki@gmail.com>
2016-02-05 23:37:05 +0900

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'users/new'
@@ -12,6 +14,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
